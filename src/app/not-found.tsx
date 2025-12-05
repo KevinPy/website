274eb3d489fe134d79ad8cd7d96b230887d7cbec
Kevin Py <pykevin@gmail.com>
2025-12-05 23:19:30 +0100

    feat(*): add NotFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import HeaderComponent from "@/components/header/header";
+
+export default function NotFound() {
+  return (
+    <>
+      <HeaderComponent />
+      <p>This page could not be found.</p>
+    </>
+  );
+}
